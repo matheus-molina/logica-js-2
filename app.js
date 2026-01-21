1625f5c1c651d35547f11d99f3cbe81d6f278499
Matheus Molina <matheus23molina@gmail.com>
2026-01-21 13:47:19 -0300

    Reiniciando jogo

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-let    nummerSecreto = gerarNumeroAleatorio();
-
+let nummerSecreto = gerarNumeroAleatorio();
+let tentativas =  1; 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
@@ -10,9 +10,33 @@ exibirTextoNaTela('p', 'Escolha um número entre 0 a 10');
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
-    alert(chute == nummerSecreto);
-}
+    if (chute == nummerSecreto){
+        exibirTextoNaTela('h1', 'Acertouu!')
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você axcertou o número secreto com ${tentativas} ${palavraTentativa}`
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+            if (chute > nummerSecreto){
+                exibirTextoNaTela('p', 'Chuta mais baixo!');
+            } else {
+                exibirTextoNaTela('p', 'Chuta mais auto!');
+            }
+        tentativas++;
+        limparCampo();
+    }
+    }
+
 
 function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo(){
+        chute = document.querySelector('input');
+        chute.value = '';
+}
+
+function reiniciarJogo(){
+    
 }
